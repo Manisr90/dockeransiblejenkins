39652fcc7cbc44e2581e73830c6402ec364bdc0b
Manisr90 <117703105+Manisr90@users.noreply.github.com>
2022-11-28 15:29:57 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export pomversion=$1
-DOCKER_TAG=${pomversion}
+export readPomVersion=$1
+DOCKER_TAG=${readPomVersion}
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 312617587281.dkr.ecr.eu-west-2.amazonaws.com
 docker build -t srmecr:${DOCKER_TAG} .
 docker tag srmecr:${DOCKER_TAG} 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srnew:${DOCKER_TAG}
