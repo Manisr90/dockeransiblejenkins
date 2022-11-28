83c78493531c6418614e5c3273cb0ee49fbf27b6
Manisr90 <117703105+Manisr90@users.noreply.github.com>
2022-11-28 17:47:57 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ export readPomVersion=$1
 DOCKER_TAG=${readPomVersion}
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 312617587281.dkr.ecr.eu-west-2.amazonaws.com
 docker build -t srmecr:${DOCKER_TAG} .
-docker tag srmecr:${DOCKER_TAG} 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr:${DOCKER_TAG}
+docker tag srmecr:${DOCKER_TAG} 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr:${DOCKER_TAG} 
 docker push 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr:${DOCKER_TAG}
 helm upgrade --kube-context=arn:aws:eks:eu-west-2:312617587281:cluster/kubesrm --namespace default --install demohelm --set image.repository=312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr,image.tag=${DOCKER_TAG},timestamp=`date +t%s` /root/srhelm --debug --wait
 
