6e583936f3f1d164703a308ac4471a0dcdde786d
Manisr90 <117703105+Manisr90@users.noreply.github.com>
2022-11-28 17:41:19 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -6,3 +6,4 @@ docker build -t srmecr:${DOCKER_TAG} .
 docker tag srmecr:${DOCKER_TAG} 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr:${DOCKER_TAG}
 docker push 312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr:${DOCKER_TAG}
 helm upgrade --kube-context=arn:aws:eks:eu-west-2:312617587281:cluster/kubesrm --namespace default --install demohelm --set image.repository=312617587281.dkr.ecr.eu-west-2.amazonaws.com/srmecr,image.tag=${DOCKER_TAG},timestamp=`date +t%s` /root/srhelm --debug --wait
+
